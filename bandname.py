37f42cec22e9969db3d449e5999b12734f832756
Samuel Hernandez <samuel.yagami@gmail.com>
2022-05-18 18:33:36 -0300

    band name

---
+++ bandname.py
@@ -0,0 +1,6 @@
+# This is the exercise from my Python course, call it Band Name Generator!
+
+print(f'Welcome to the Band Name Generator.')
+name = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print(f"Your band name could be {name + ' ' + pet}")
